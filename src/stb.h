67fdf0a72672f2ff8ffe29beed5628897184c12a
Kristian Gutic <kristian.gutic@gmail.com>
2023-07-02 23:28:27 +0200

    Updating paths

--- src/stb.h
+++ src/stb.h
@@ -45,6 +45,6 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "../lib/stb_image.h"
-#include "../lib/stb_image_write.h"
+#include "../libs/stb_image.h"
+#include "../libs/stb_image_write.h"
 
