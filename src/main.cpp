c2b26ac4e9331848e20b8d112b181857b88b5567
Kristian Gutic <kristian.gutic@gmail.com>
2023-07-02 23:28:56 +0200

    Adding SFML, stb is not necessary anymore

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,50 @@
 #include <iostream>
 #include <filesystem>
 
-#include "stb.h"
+#include "SFML/Graphics.hpp"
+#include "SFML/System.hpp"
+
 
  int main()
  {
-    std::cout << "Image\n";
-    std::string imgage_path = (std::filesystem::current_path() / std::filesystem::path("images/erika.png")).string();
-    
-    const char *img = imgage_path.c_str();
-    
-    int w;
-    int h;
-    int comp;
-    stbi_info(img, &w, &h, &comp);
-    // unsigned char* image = stbi_load(img, &w, &h, &comp, STBI_grey);
 
-    std::cout << "Width: " << w 
-            << "\nHeight: " << h 
-            << "\nComp: " << comp 
-            << "\n";
+    std::filesystem::path path_to_imgs = std::filesystem::current_path() / "../../images/";
+    std::string image_path = (path_to_imgs / "erika.png").string();
+    
+
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML ");
+        
+    sf::Image image;
+    if (!(image.loadFromFile(image_path)))
+    {
+        std::cout << "Cannot load image";   //Load Image
+    }
+    
+    sf::Texture texture;
+    texture.loadFromImage(image);
+
+    sf::Sprite sprite;
+    sprite.setTexture(texture);    
+
+
+    while (window.isOpen())
+    {
+            
+            sf::Event event;
+            
+            while (window.pollEvent(event))
+            {
+                    if (event.type == sf::Event::Closed)
+                    {
+                        window.close();
+                    }
+            }
+                    
+                window.clear();
+                window.draw(sprite);
+                window.display();
+            }
 
     return 0;
  }
