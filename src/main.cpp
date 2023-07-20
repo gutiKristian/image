aedf7474056f81045e64503e2fb77684ed19997a
Kristián Gutič <kristian.gutic@gmail.com>
2023-07-20 16:35:30 +0200

    Introducing GlobalBuffer, singleton object to manage the state of buffer (texture, sprite)

--- src/main.cpp
+++ src/main.cpp
@@ -8,42 +8,96 @@
 #include "SFML/System.hpp"
 
 import Basics;
+import graphics;
 
- int main()
- {
+/**
+ * Object of global buffer, helps us abstract the variables that are
+ * used to draw images that are being created onto the screen.
+ * I didn't put sf::Image there so it is easier to work with multiple
+ * images and by just calling write_image we display what we want.
+ */
+struct GlobalBuffer
+{
 
-    std::filesystem::path path_to_imgs = std::filesystem::current_path() / "../../images/";
+    std::unique_ptr<sf::Texture> p_texture = std::make_unique<sf::Texture>();
+    std::unique_ptr<sf::Sprite> p_sprite = std::make_unique<sf::Sprite>();
+
+    static GlobalBuffer& GetInstance()
+    {
+        static GlobalBuffer instance;
+        return instance;
+    }
+
+    /**
+     * When working with images, we do the operations on them (they are stored in RAM)
+     * and then, show them, update is expensive since the
+     * data has to be moved to the GPU VRAM --- (not that it matters here, but this is a sidenote)
+     * @param img Image that is being moved to GPU and displayed
+     */
+    void write_image(const sf::Image& img)
+    {
+        p_texture->loadFromImage(img);
+        p_sprite->setTexture(*p_texture);
+    }
+
+    /**
+     * Prefer buffer.draw() to *buffer.p_sprite
+     * @return Constant reference to sprite object
+     */
+    sf::Sprite const& draw() const
+    {
+        return *p_sprite;
+    }
+
+    GlobalBuffer(const GlobalBuffer&) = delete;
+    void operator=(const GlobalBuffer&)  = delete;
+
+private:
+    GlobalBuffer() = default;
+
+};
+
+
+
+int main()
+{
+    GlobalBuffer& buffer = GlobalBuffer::GetInstance();
+
+
+    const int WIDTH = 1280;
+    const int HEIGHT = 720;
+
+
+    std::filesystem::path path_to_imgs = std::filesystem::current_path() / "../images/";
     std::string image_path = (path_to_imgs / "erika.png").string();
 
-    // In future when working with images,
-    // we load them, do the operations and then show them, update is expensive since the
-    // data has to be moved to the GPU VRAM --- (not that it matters here, but this is a sidenote)
-
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML", sf::Style::Close);
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "SFML", sf::Style::Close);
     ImGui::SFML::Init(window);
 
+    // Image for vision -- ignored for now
+    //
+    //    if (!(image.loadFromFile(image_path)))
+    //    {
+    //        std::cout << "Cannot load image";   //Load Image
+    //    }
 
-    sf::Image image;
-    if (!(image.loadFromFile(image_path)))
-    {
-        std::cout << "Cannot load image";   //Load Image
-    }
-    
-    sf::Texture texture;
-    texture.loadFromImage(image);
+    // Canvas for graphics, this is a buffer that is modified and written on
+    sf::Image canvas;
+    canvas.create(WIDTH, HEIGHT, sf::Color::White);
 
-    sf::Sprite sprite;
-    sprite.setTexture(texture);    
+    // Display the buffer
+    buffer.write_image(canvas);
 
+    // Main loop
     sf::Clock delta_clock;
     while (window.isOpen())
     {
-        
+
         sf::Event event;
-        
+
         while (window.pollEvent(event))
         {
-            ImGui::SFML::ProcessEvent(event);   
+            ImGui::SFML::ProcessEvent(event);
             if (event.type == sf::Event::Closed)
             {
                 window.close();
@@ -57,12 +111,13 @@ import Basics;
         ImGui::Text("Text!");
         ImGui::End();
 
+
         window.clear();
-        window.draw(sprite);
-        ImGui::SFML::Render(window);
+        window.draw(buffer.draw()); // Render buffer(image that has been modified)
+        ImGui::SFML::Render(window); // Render UI
         window.display();
     }
 
     ImGui::SFML::Shutdown();
     return 0;
- }
+}
