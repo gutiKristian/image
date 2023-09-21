1d64570bd700000639ca9d590abf0b6a15ff53c4
Kristián Gutič <kristian.gutic@gmail.com>
2023-09-21 10:50:09 +0200

    Adding new file

---
+++ src/core/layer/Layer.h
@@ -0,0 +1,18 @@
+namespace core
+{
+    /*
+     * OpenGL code is represented as a layer that is being drawn
+     * on a window. Layer is managed by an application.
+     */
+    class Layer
+    {
+    protected:
+        Layer() = default;
+    public:
+        virtual void OnAttach() = 0;
+        virtual void OnUpdate() = 0;
+        virtual void OnDetach() = 0;
+        virtual void OnEvent(Event&) = 0;
+        virtual void OnResize(int, int) = 0;
+    };   
+}
